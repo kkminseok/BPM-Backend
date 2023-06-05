09502c7f8859f362023acf7e8babace72594f858
minseokkang <aaaa727@khu.ac.kr>
2023-06-05 23:50:50 +0900

    fix : review recommends null 들어올 수 있도록

--- src/main/java/d83t/bpmbackend/domain/aggregate/studio/dto/ReviewRequestDto.java
+++ src/main/java/d83t/bpmbackend/domain/aggregate/studio/dto/ReviewRequestDto.java
@@ -4,6 +4,7 @@ import d83t.bpmbackend.domain.aggregate.profile.entity.Profile;
 import d83t.bpmbackend.domain.aggregate.studio.entity.Review;
 import d83t.bpmbackend.domain.aggregate.studio.entity.Studio;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.annotation.Nullable;
 import lombok.*;
 
 import java.util.List;
@@ -15,6 +16,7 @@ import java.util.List;
 @Schema(description = "리뷰 작성 요청 DTO")
 public class ReviewRequestDto {
     private Double rating;
+    @Nullable
     private List<String> recommends;
     private String content;
 
